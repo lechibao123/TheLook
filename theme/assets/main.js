29c0995cee1133ec04ee326fa0634be2b2bc9af4
lechibao123 <lechibao0411@gmail.com>
2021-05-25 15:52:48 +0700

    oke

--- theme/assets/main.js
+++ theme/assets/main.js
@@ -20,7 +20,7 @@ document.addEventListener(
 
     // Event Header
     document.getElementById('openNav').addEventListener('click', function(){
-        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("mySidenav").style.width = "300px";
         document.getElementById("overlay").style.display = "block";
     });
 
@@ -33,6 +33,39 @@ document.addEventListener(
         document.getElementById("mySidenav").style.width = "0";
         document.getElementById("overlay").style.display = "none";
     });
+
+    let parent = document.getElementsByClassName('its-parent');
+    for (let i = 0; i < parent.length; i++) {
+        parent[i].addEventListener('click', function(){
+            console.log(this)
+            let children = this.nextElementSibling;
+            console.log(children.offsetHeight )
+
+            this.classList.toggle("downed");
+            console.log(true);
+        })
+    }
+
+    var slideOpen = true;
+    var heightChecked = false;
+    var initHeight = 0;
+    
+    function slideToggle(height) {
+        if(!heightChecked) {
+            initHeight = height;
+            heightChecked = true;
+        }
+        if(slideOpen) {
+            slideOpen = false;
+            mdiv.style.height = '0px';
+        }
+        else {
+            slideOpen = true;
+            mdiv.style.height = initHeight + 'px';
+        }
+    }
+
+
 
     console.log(true);
     console.log(123);
